7447a8f1a72070f16b03850d0e9cec5f9a874bc6
DESKTOP-QKF8L2Q\Punkzen <aurelien.montero@gmail.com>
2017-04-30 14:05:32 +0200

    Wwise update

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMB_FOREST_START = 1494607340U;
+        static const AkUniqueID AMB_FOREST_STOP = 333335808U;
+        static const AkUniqueID AMB_GRASS_START = 1396033513U;
+        static const AkUniqueID AMB_GRASS_STOP = 3242293763U;
+        static const AkUniqueID AVATAR_WINGS_FLAP = 445196183U;
+        static const AkUniqueID CRYSTAL_GET = 1128271304U;
+        static const AkUniqueID CRYSTAL_GLOW = 2431342447U;
+        static const AkUniqueID DUST_START = 2898349872U;
+        static const AkUniqueID DUST_STOP = 1300825244U;
+        static const AkUniqueID FLOWER_GROW = 261655774U;
+        static const AkUniqueID GROUND_IMPACT_HARD = 937145663U;
+        static const AkUniqueID GROUND_IMPACT_LIGHT = 3648287500U;
+        static const AkUniqueID GROUND_IMPACT_MEDIUM = 84143797U;
+        static const AkUniqueID LEAF_IMPACT_HARD = 350283794U;
+        static const AkUniqueID LEAF_IMPACT_LIGHT = 3870513327U;
+        static const AkUniqueID LEAF_IMPACT_MEDIUM = 1820380804U;
+        static const AkUniqueID RIVER_START = 2427671096U;
+        static const AkUniqueID RIVER_STOP = 717165636U;
+        static const AkUniqueID ROCK_IMPACT_HARD = 3237213113U;
+        static const AkUniqueID ROCK_IMPACT_LIGHT = 1555724626U;
+        static const AkUniqueID ROCK_IMPACT_MEDIUM = 1649834555U;
+        static const AkUniqueID SPEED_START = 2040520663U;
+        static const AkUniqueID TOUCH_GRASS_START = 3076372940U;
+        static const AkUniqueID TOUCH_GRASS_STOP = 557071456U;
+        static const AkUniqueID TOUCH_LEAF_START = 1456296260U;
+        static const AkUniqueID TOUCH_LEAF_STOP = 2199972856U;
+        static const AkUniqueID TOUCH_WATER_START = 2988057335U;
+        static const AkUniqueID TOUCH_WATER_STOP = 4224829277U;
+        static const AkUniqueID TREE_IMPACT_HARD = 3292957914U;
+        static const AkUniqueID TREE_IMPACT_LIGHT = 1806523559U;
+        static const AkUniqueID TREE_IMPACT_MEDIUM = 1471919116U;
+        static const AkUniqueID WATERFALL_START = 121420192U;
+        static const AkUniqueID WATERFALL_STOP = 2656094668U;
+        static const AkUniqueID WIND_GUST = 431171811U;
+        static const AkUniqueID WIND_HIGH_START = 548087415U;
+        static const AkUniqueID WIND_HIGH_STOP = 344778205U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SPEED_ACTIVATED = 3755642206U;
+        static const AkUniqueID UP_DOWN = 1233584889U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID FOREST = 491961918U;
+        static const AkUniqueID GENERAL = 133642231U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
