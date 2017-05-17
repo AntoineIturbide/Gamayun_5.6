be0e44b75ad1840a37f0e10276262a83e893f312
Ulkan <camilleleonard.j@gmail.com>
2017-05-17 15:06:31 +0200

    ++++++

---
+++ Assets/Game/Scripts/DayNight/DayNightTransition.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tweening;
+
+public class DayNightTransition : MonoBehaviour {
+
+    [System.NonSerialized]
+    public Tween<float> mDayNight = new Tween<float>(
+        0,
+        UnityTick.UPDATE,
+        Easing.DynaEase.Out
+        );
+
+	public float mTransitionTime = 1;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void OnDestroy()
+    {
+        Close();
+    }
+
+    private void Init()
+    {
+        mDayNight = new Tween<float>(
+        0,
+        UnityTick.UPDATE,
+        Easing.DynaEase.Linear,
+        1f
+        );
+
+        mDayNight.change_callback += OnDayNightChange;
+
+        Shader.SetGlobalFloat("_GamayunFlash", 2.2f);
+    }
+
+    private void Close()
+    {
+        mDayNight.change_callback -= OnDayNightChange;
+    }
+
+    private void OnDayNightChange()
+    {
+        Shader.SetGlobalFloat("_Gamayun", mDayNight.get_value());
+    }
+
+    public void StartTransitioningToDay()
+    {
+        mDayNight.SetTarget(1f);
+		mDayNight.time_factor = 1f / mTransitionTime;
+    }
+    public void StartTransitioningToNight()
+    {
+		mDayNight.SetTarget(0f);
+		mDayNight.time_factor = 1f / mTransitionTime;
+    }
+}
---
+++ Assets/Game/Scripts/DayNight/Editor/DayNightTransitionEditor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Tweening;
+
+[CustomEditor(typeof(DayNightTransition))]
+[CanEditMultipleObjects]
+public class DayNightTransitionEditor : Editor
+{
+    private DayNightTransition Target()
+    {
+        return (DayNightTransition)target;
+    }
+
+    private void OnEnable()
+    {
+        var t = Target();
+
+        t.mDayNight.change_callback += Repaint;
+    }
+
+    private void OnDisable()
+    {
+        var t = Target();
+
+        t.mDayNight.change_callback -= Repaint;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        var t = Target();
+
+        serializedObject.Update();
+
+        DrawDefaultInspector();
+
+        DrawDayNightButtons();
+        
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawDayNightButtons()
+    {
+        var t = Target();
+
+        //GUILayout.Label(t.mDayNight.get_value().ToString());
+        GUILayout.HorizontalSlider(t.mDayNight.get_value(), 0, 1);
+
+        if (t.mDayNight.target_value < 0.5f)
+        {
+            if(GUILayout.Button("Transition to day"))
+            {
+                t.StartTransitioningToDay();
+            }
+        }
+        else
+        {
+            if (GUILayout.Button("Transition to night"))
+            {
+                t.StartTransitioningToNight();
+            }
+        }
+    }
+
+    private void OnSceneGUI()
+    {   
+        var t = Target();
+    }
+}
