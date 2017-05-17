ffec3d02ae453531be916f7969e3abaf6edca091
Lucas Leplat <mesk.utp>
2017-05-17 15:51:04 +0200

    Event zones + clouds

---
+++ Assets/Game/Scripts/WwiseEventRegion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WwiseEventRegion : MonoBehaviour
+{
+	public string eventName;
+	public string eventNameEnd;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			AkSoundEngine.PostEvent(eventName, gameObject);
+		}
+		
+	}
+
+	private void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			AkSoundEngine.PostEvent(eventNameEnd, gameObject);
+		}
+	}
+}
---
+++ Assets/[Game]/Feature/Environnement/Scripts/ExplodeOnCollide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnCollide : MonoBehaviour
+{
+	public GameObject particleDestroy;
+
+	private void OnTriggerEnter (Collider other)
+	{
+		if(particleDestroy != null)
+		{
+			Instantiate(particleDestroy, transform.position, Quaternion.identity);
+		}
+		Destroy(gameObject);
+	}
+}
