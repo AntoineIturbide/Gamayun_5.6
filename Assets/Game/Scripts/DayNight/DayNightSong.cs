66344d9b944d5773a35aa8fdf1f7a67fa4f5336d
MrMesk <mesk.utp@gmail.com>
2017-05-19 01:34:16 +0200

    Toudoum tougoudoum

--- Assets/Game/Scripts/DayNight/DayNightSong.cs
+++ Assets/Game/Scripts/DayNight/DayNightSong.cs
@@ -11,18 +11,22 @@ public class DayNightSong : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		AkSoundEngine.PostEvent("Play" + night, gameObject);
+		//AkSoundEngine.PostEvent("Play" + night, gameObject);
 		AkSoundEngine.SetState("MUSIC", "NIGHT");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.A))
+	void Update()
+	{
+		if(Input.GetKeyDown (KeyCode.Keypad1))
 		{
-			StartCoroutine(PlayTransition());
+			SetTransition();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Keypad2))
+		{
+			SetDayTime();
 		}
 	}
-
 	public void SetTransition()
 	{
 		AkSoundEngine.SetState("MUSIC", "TRANSITION");
